bf7221e1706f383bd1c6fcbc84f1063e49479989
Santhosh Kulasekar <santhosh.mk@gmail.com>
2018-02-08 11:50:20 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/openstack', secret: process.env.OPEN_STACK_SECRET })
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(process.env.PORT || 8081)
+
+handler.on('error', function (err) {
+  cconsole.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+})
+
+handler.on('', function (event) {
+  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
+})
